bdeb6f8498b7e04c2dbdb9a77b2e0aa172b60733
dwaldo <dwaldo@naver.com>
2022-12-10 16:20:59 +0900

    init

---
+++ src/app/shared/models/campaigns.model.ts
@@ -0,0 +1,40 @@
+export interface Campaign {
+  title: string;
+  label: string;
+  platform: string;
+  npcs: NPC[];
+  logs: Log[];
+}
+
+export interface NPC {
+  name: string;
+  avatar: string;
+}
+
+export interface Log {
+  index: string;
+  name: string;
+  images?: Image[];
+  interfaces?: Image[];
+}
+
+export interface Image {
+  id: string;
+  file: string;
+}
+
+export const Campaigns = [
+  {
+    title: 'S3',
+    label: '엑스페리온 시즌 3',
+    platform: 'ccfolia',
+    npcs: [
+    ],
+    logs: [
+      {
+        index: '0',
+        name: '튜토리얼',
+      },
+    ],
+  },
+];
