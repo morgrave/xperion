60d41b5679bb9173a5a580a0c69aefb7e7c9892d
dwaldo <dwaldo@naver.com>
2023-10-14 02:46:46 +0900

    0722

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -350,6 +350,11 @@ export const Campaigns = [
     ],
     logs: [
       {
+        index: '2023072201',
+        name: '아스트로 ~행성 탐사자들~',
+        video: 'https://www.youtube.com/embed/V7w0NlCVFw8',
+      },
+      {
         index: '2023092101',
         name: '베네딕타 대수해 중망호',
         video: 'https://www.youtube.com/embed/E-gpojGJwM8',
