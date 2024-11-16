b4d94381d2e208b2d4d7cf5801fddf280a68fae6
dwaldo <dwaldo@naver.com>
2024-11-16 18:15:16 +0900

    'deploy'

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -1013,6 +1013,11 @@ export const Campaigns = [
         name: '죽음의 무도 1',
         video: 'https://www.youtube.com/embed/jUTpg48MlOs',
       },
+      {
+        index: '2024111502',
+        name: '죽음의 무도 2',
+        video: 'https://www.youtube.com/embed/miRR9Rx4XN8',
+      },
     ],
   },
   {
@@ -1125,6 +1130,11 @@ export const Campaigns = [
       {
         index: '2024110201',
         name: '페리아의 문장 1팀 21화: 헌신자의 매장터',
+        video: 'https://www.youtube.com/embed/z_UiQYrZQ3E',
+      },
+      {
+        index: '2024111501',
+        name: '페리아의 문장 1팀 22화: 헌신자의 매장터',
       },
       {
         index: '2024060201',
@@ -1233,7 +1243,8 @@ export const Campaigns = [
       },
       {
         index: '2024111501',
-        name: '페리아의 문장 2팀 21화: 불타는 감시터',
+        name: '페리아의 문장 2팀 22화: 불타는 감시터',
+        video: 'https://www.youtube.com/embed/5NX7f81eJyk',
       },
     ],
   },
