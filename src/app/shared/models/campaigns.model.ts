e19338e83be4271720bfc6c2f6fe20260091a6ea
dwaldo <dwaldo@naver.com>
2025-07-19 17:22:26 +0900

    'deploy'

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -1632,6 +1632,10 @@ export const Campaigns = [
         video: 'https://www.youtube.com/embed/--FAYW4Yp-k',
       },
       {
+        index: '2025071901',
+        name: '페리아의 문장 1팀 51화: 추억을 잃은 자',
+      },
+      {
         index: '2024060201',
         name: '페리아의 문장 2팀 1화: 사원의 섬',
         video: 'https://www.youtube.com/embed/_C-Q4An3R14',
