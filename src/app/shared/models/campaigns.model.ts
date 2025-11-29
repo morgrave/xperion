87fc52547b6e66cc6f6a78362414afbdcb5d3145
dwaldo <dwaldo@naver.com>
2025-11-29 17:17:18 +0900

    'deploy'

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -1910,6 +1910,10 @@ export const Campaigns = [
         name: '페리아의 문장 1팀 64화: 우상을 잃은 자',
       },
       {
+        index: '2025112901',
+        name: '페리아의 문장 1팀 65화: 우상을 잃은 자',
+      },
+      {
         index: '2024060201',
         name: '페리아의 문장 2팀 1화: 사원의 섬',
         video: 'https://www.youtube.com/embed/_C-Q4An3R14',
