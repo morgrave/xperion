641189e8fb1413986eaf059eb6873a5762623fae
dwaldo <dwaldo@naver.com>
2026-01-24 16:45:12 +0900

    'deploy'

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -2007,6 +2007,10 @@ export const Campaigns = [
         name: '페리아의 문장 1팀 70화: 자유를 잃은 자',
       },
       {
+        index: '2026012401',
+        name: '페리아의 문장 1팀 71화: 근원을 잃은 자',
+      },
+      {
         index: '2024060201',
         name: '페리아의 문장 2팀 1화: 사원의 섬',
         video: 'https://www.youtube.com/embed/_C-Q4An3R14',
