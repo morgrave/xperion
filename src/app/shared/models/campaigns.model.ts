186ff15956d89ed1b9e68f1606c759f22dc48597
dwaldo <dwaldo@naver.com>
2026-03-29 23:47:57 +0900

    'deploy'

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -1277,6 +1277,10 @@ export const Campaigns = [
         name: '아스트로 ~행성 탐사자들~ 4-8',
       },
       {
+        index: '2026032902',
+        name: '아스트로 ~행성 탐사자들~ 4-8',
+      },
+      {
         index: '2023101901',
         name: '아스트로 ~ 누군가의 히어로 1팀 1일차',
         video: 'https://www.youtube.com/embed/473PXRavHts',
@@ -2654,6 +2658,10 @@ export const Campaigns = [
       },
       {
         index: '2026032201',
+        name: '시트론 섬의 개척자들 1팀 6화',
+      },
+      {
+        index: '2026032901',
         name: '시트론 섬의 개척자들 1팀 6화',
       },
       {
