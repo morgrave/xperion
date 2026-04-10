744e6914d0df9428c990f231c366f858ab5222bc
dwaldo <dwaldo@naver.com>
2026-04-11 00:05:01 +0900

    'deploy'

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -2568,6 +2568,11 @@ export const Campaigns = [
         name: '페리아의 문장 2팀 81화: 페리아의 문장',
         video: 'https://www.youtube.com/embed/ml4ukSfGdxM',
       },
+      {
+        index: '2026041001',
+        name: '페리아의 문장 2팀 82화: 페리아의 문장',
+        video: 'https://www.youtube.com/embed/cCFBzwc0CTU',
+      },
     ],
   },
   {
