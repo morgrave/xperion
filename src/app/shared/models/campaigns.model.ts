e6270fa85acaa6fb87a9dd7fbe8bf0b7cbd9d731
dwaldo <dwaldo@naver.com>
2026-05-10 18:41:50 +0900

    'deploy'

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -2756,6 +2756,10 @@ export const Campaigns = [
         name: '시트론 섬의 개척자들 1팀 12화',
       },
       {
+        index: '2026051001',
+        name: '시트론 섬의 개척자들 1팀 13화',
+      },
+      {
         index: '2025121302',
         name: '시트론 섬의 개척자들 2팀 0화',
         video: 'https://www.youtube.com/embed/ApysQi9J888',
