7c620a626dd4f93c54886dc59e3d37b3f89d4a7d
dwaldo <dwaldo@naver.com>
2026-06-27 17:13:18 +0900

    'deploy'

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -2263,6 +2263,10 @@ export const Campaigns = [
         name: '페리아의 문장 1팀 88화: 에필로그',
       },
       {
+        index: '2026062701',
+        name: '페리아의 문장 1팀 89화: 에필로그',
+      },
+      {
         index: '2024060201',
         name: '페리아의 문장 2팀 1화: 사원의 섬',
         video: 'https://www.youtube.com/embed/_C-Q4An3R14',
